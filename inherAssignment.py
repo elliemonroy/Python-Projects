57b2a8d428b0ed959de4c32f3e49700b69dbe462
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-03-08 10:12:08 -0800

    inheritance_assignment

---
+++ inherAssignment.py
@@ -0,0 +1,23 @@
+
+# Created a class Info that requires fname,lname, address and DOB
+class Info:
+    fname = ' '
+    lname = ' '
+    Address = ' '
+    DOB =
+    
+
+# Created a class Student that uses Info's attributes and added two new attributes
+class Student(Info):
+    Grade = " "
+    GPA = 
+
+# Created a class Teacher that uses Info's attributes as well and added two new attributes
+class Teacher(Info):
+    Subject = ' '
+    gradeLevel = 
+    
+    
+    
+    
+    
